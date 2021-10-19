a5401ef3fb9ddae4558652d3537bea6dc0537c71
Guilherme Ivo <guilhermeivob@gmail.com>
2021-10-19 10:43:17 -0300

    Atualização no estilo dos popup

--- Tupa-Web/Common/Helpers/PopUpHelpers.cs
+++ Tupa-Web/Common/Helpers/PopUpHelpers.cs
@@ -7,9 +7,26 @@ namespace Tupa_Web.Common.Helpers
 {
     public class PopUpHelpers
     {
-        public static string PopUp()
+        public static string PopUp(string title, string[] descriptionBody, string button)
         {
-            return "";
+            return String.Format(
+                @"<div class=""popup_wrapper"" onclick=""OnClickPopUp(this, event);"">
+                    <div class=""popup card"">
+                        <div class=""title"">
+                            <h2>{0}</h2>
+
+                            <div class=""close_button""></div>
+                        </div>
+                        <div class=""body"">
+                            <p>{1}</p>
+                            <input type=""text""/>
+                            <button>{3}</button>
+                        </div>
+                    </div>
+                </div>",
+                title,
+                descriptionBody[0],
+                button);
         }
     }
 }
---
+++ Tupa-Web/Scripts/PopUp.js
@@ -0,0 +1,8 @@
+﻿function OnClickPopUp(document, event) {
+    if (document.children[0].contains(event.target)){
+        // Click in box
+    } else{
+        // Clicked outside the box
+        document.classList.toggle('disabled')
+    }
+}
